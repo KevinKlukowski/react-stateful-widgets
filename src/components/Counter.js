210404d8da21d44b8de493f4bb027fb1024cfe95
Kevin Klukowski <kklukowski12@gmail.com>
2021-01-18 20:39:33 -0500

    make small changes, MVP

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -76,7 +76,7 @@ export default function Counter() {
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number {count} is even {/* STEP 3 DONE*/}
+        Number {count} is {count % 2 === 0 ? 'even': 'odd'} {/* STEP 3 DONE*/}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
